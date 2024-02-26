296e23621fe3550e6523e73f4dc548727bafd76d
Кирилл Зайцев <kirill@Kirills-MacBook-Air.local>
2024-02-26 17:42:54 +0600

    feat: added static media support

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,17 @@ import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import * as config from '../config/config.json';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [MongooseModule.forRoot(config.MONGOURL), ProductsModule],
+  imports: [
+    MongooseModule.forRoot(config.MONGOURL), ProductsModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'), // `public` - папка в корне проекта
+      serveRoot: '/public/', // Опционально: URL-путь, по которому будут доступны файлы
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
